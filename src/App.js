d02e1531b18468e60c2d9682c1dd5fdde87248ac
Kunsang Tsering <cozyboykt@Kunsangs-MacBook-Air.local>
2025-10-18 21:06:06 -0400

    Add SplitIt feature and LandingPage for Auri financial management platform

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Home from "./components/Home";
 import SignIn from "./components/SignIn";
 import Cart from "./components/Cart";
 import CreateAccount from "./components/CreateAccount";
+import SplitIt from "./components/SplitIt";
 
 function App() {
   const navigate = useNavigate();
@@ -26,7 +27,7 @@ function App() {
           <Route path="*" element={<Home />} />
           <Route path="signin" element={<SignIn />} />
           <Route path="/createaccount" element={<CreateAccount />} />
-
+          <Route path="/splitit" element={<SplitIt />} />
           <Route path="cart" element={<Cart />} />
         </Routes>
       </div>
